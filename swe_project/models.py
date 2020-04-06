e00b542866f2552e0a432aac9b007d7a688e7cc4
Zaid Rahman <zaidrahman0@gmail.com>
2020-04-06 21:20:03 +0400

    Fixed model errors

--- swe_project/models.py
+++ swe_project/models.py
@@ -9,14 +9,14 @@ class User(db.Model):
     university_id=db.Column(db.Integer,nullable=False)
     st_fa=db.Column(db.Boolean,nullable=False)
 
-class Uni_admin(User,db.Model):
+class Uni_admin(db.Model):
     uni_ad_id=db.Column(db.Integer,primary_key=True)
     uni_id=db.Column(db.Integer,db.ForeignKey('university.id'),nullable=False)
     user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
     firstname=db.Column(db.String(20),nullable=False)
     lastname=db.Column(db.String(20),nullable=False)
 
-class Student(User,db.Model):
+class Student(db.Model):
     student_id=db.Column(db.Integer,primary_key=True)
     user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
     firstname=db.Column(db.String(20),nullable=False)
@@ -24,7 +24,7 @@ class Student(User,db.Model):
     age=db.Column(db.Integer,nullable=False)
     academic_level=db.Column(db.String(30),nullable=False)
 
-class Faculty(user,db.Model):
+class Faculty(db.Model):
     faculty_id=db.Column(db.Integer,primary_key=True)
     user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
     firstname=db.Column(db.String(20),nullable=False)
@@ -32,23 +32,23 @@ class Faculty(user,db.Model):
 
 class University(db.Model):
     id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(40),nullable=False,Unique=True)
+    name=db.Column(db.String(40),nullable=False,unique=True)
     country=db.Column(db.String(40),nullable=False)
 
 class College(db.Model):
     college_id=db.Column(db.Integer,primary_key=True)
     uni_id=db.Column(db.Integer,db.ForeignKey('university.id'),nullable=False)
-    name=db.Column(db.String(40),nullable=False,Unique=True)
+    name=db.Column(db.String(40),nullable=False,unique=True)
 
 class Department(db.Model):
     department_id=db.Column(db.Integer,primary_key=True)
     uni_id=db.Column(db.Integer,db.ForeignKey('university.id'),nullable=False)
-    name=db.Column(db.String(40),nullable=False,Unique=True)
+    name=db.Column(db.String(40),nullable=False,unique=True)
 
 class Courses(db.Model):
     course_id=db.Column(db.String(20),primary_key=True)
     uni_id=db.Column(db.Integer,db.ForeignKey('university.id'),nullable=False)
-    name=db.Column(db.String(40),nullable=False,Unique=True)
+    name=db.Column(db.String(40),nullable=False,unique=True)
     faculty_id=db.Column(db.Integer,db.ForeignKey('faculty.faculty_id'),nullable=False)
     
 class Notes(db.Model):
@@ -60,56 +60,10 @@ class Notes(db.Model):
 class Rating(db.Model):
     rating_id=db.Column(db.Integer,primary_key=True)
     note_id=db.Column(db.Integer,db.ForeignKey('notes.note_id'),nullable=False)
-    rating=db.Coumn(db.Integer,nullable=False)
+    rating=db.Column(db.Integer,nullable=False)
 
 class Student_Course(db.Model):
     student_course_id=db.Column(db.Integer,primary_key=True)
-    student_id=db.Column(db.Integer,db.ForeignKey('Student.student_id'),nullable=False)
+    student_id=db.Column(db.Integer,db.ForeignKey('student.student_id'),nullable=False)
     course_id=db.Column(db.Integer,db.ForeignKey('courses.course_id'),nullable=False)
     
-
-
-#Notes to check from:
-"""  SWE Database Diagram
-
-System admin- admin,admin
-
-User
- id 		username	password 	email 	university_id	student/fa    
-
-Uni admin(user)
-id 	first name		last name		Gender
-
-Student(user)
-id	first name	last name		gender  	academic_level	age
-
-Faculty(user)
-id	first name		lastname		gender	age
-
-Student-Course(user, course)
-studentid	(fk)	courseid(fk)
-
-University
-id	name	city		country		uniadmin_id
-
-College(university)
-uni_id	 id_college	name	
-
-Department(university)
-uni_id	dept_id	collegeid		name
-
-Course
-deptid	courseid		code	name
-
-
-Faculty-courses
-facultyid		courseid		
-
-
-Notes
-note_id	course_id	  studentid	note(blob)	approved/disapp
-
-
-Rating
-ratingid	noteid	studentid	   rating(/10)"""
-
