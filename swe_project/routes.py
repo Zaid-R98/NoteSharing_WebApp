dbe33e3807fb7d231bb21a80308ca1955648e7e5
Zaid Rahman <zaidrahman0@gmail.com>
2020-04-06 23:20:57 +0400

    initial css files added

--- swe_project/routes.py
+++ swe_project/routes.py
@@ -2,3 +2,8 @@ from flask import render_template, url_for, flash, redirect
 from swe_project import app
 #from testproject_2.models import User,Post
 #from testproject_2.forms import RegistrationForm,LoginForm
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
