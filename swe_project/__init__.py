e056ddc768d98eba547bf4b8d2cdfa06f53a2772
Zaid Rahman <zaidrahman0@gmail.com>
2020-04-06 19:57:44 +0400

    Added initial models

--- swe_project/__init__.py
+++ swe_project/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
--- swe_project/models.py
+++ swe_project/models.py
@@ -1,2 +1,115 @@
 from swe_project import db
 from datetime import datetime
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(25),nullable=False,unique=True)
+    password=db.Column(db.String(150),nullable=False)
+    email=db.Column(db.String(40),nullable=False,unique=True)
+    university_id=db.Column(db.Integer,nullable=False)
+    st_fa=db.Column(db.Boolean,nullable=False)
+
+class Uni_admin(User,db.Model):
+    uni_ad_id=db.Column(db.Integer,primary_key=True)
+    uni_id=db.Column(db.Integer,db.ForeignKey('university.id'),nullable=False)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    firstname=db.Column(db.String(20),nullable=False)
+    lastname=db.Column(db.String(20),nullable=False)
+
+class Student(User,db.Model):
+    student_id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    firstname=db.Column(db.String(20),nullable=False)
+    lastname=db.Column(db.String(20),nullable=False)
+    age=db.Column(db.Integer,nullable=False)
+    academic_level=db.Column(db.String(30),nullable=False)
+
+class Faculty(user,db.Model):
+    faculty_id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    firstname=db.Column(db.String(20),nullable=False)
+    lastname=db.Column(db.String(20),nullable=False)
+
+class University(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(40),nullable=False,Unique=True)
+    country=db.Column(db.String(40),nullable=False)
+
+class College(db.Model):
+    college_id=db.Column(db.Integer,primary_key=True)
+    uni_id=db.Column(db.Integer,db.ForeignKey('university.id'),nullable=False)
+    name=db.Column(db.String(40),nullable=False,Unique=True)
+
+class Department(db.Model):
+    department_id=db.Column(db.Integer,primary_key=True)
+    uni_id=db.Column(db.Integer,db.ForeignKey('university.id'),nullable=False)
+    name=db.Column(db.String(40),nullable=False,Unique=True)
+
+class Courses(db.Model):
+    course_id=db.Column(db.String(20),primary_key=True)
+    uni_id=db.Column(db.Integer,db.ForeignKey('university.id'),nullable=False)
+    name=db.Column(db.String(40),nullable=False,Unique=True)
+    faculty_id=db.Column(db.Integer,db.ForeignKey('faculty.faculty_id'),nullable=False)
+    
+class Notes(db.Model):
+    note_id=db.Column(db.Integer,primary_key=True)
+    course_id=db.Column(db.Integer,db.ForeignKey('courses.course_id'),nullable=False)
+    student_id=db.Column(db.Integer,db.ForeignKey('student.student_id'),nullable=False)
+    Note=db.Column(db.LargeBinary,nullable=False)
+
+class Rating(db.Model):
+    rating_id=db.Column(db.Integer,primary_key=True)
+    note_id=db.Column(db.Integer,db.ForeignKey('notes.note_id'),nullable=False)
+    rating=db.Coumn(db.Integer,nullable=False)
+
+class Student_Course(db.Model):
+    student_course_id=db.Column(db.Integer,primary_key=True)
+    student_id=db.Column(db.Integer,db.ForeignKey('Student.student_id'),nullable=False)
+    course_id=db.Column(db.Integer,db.ForeignKey('courses.course_id'),nullable=False)
+    
+
+
+#Notes to check from:
+"""  SWE Database Diagram
+
+System admin- admin,admin
+
+User
+ id 		username	password 	email 	university_id	student/fa    
+
+Uni admin(user)
+id 	first name		last name		Gender
+
+Student(user)
+id	first name	last name		gender  	academic_level	age
+
+Faculty(user)
+id	first name		lastname		gender	age
+
+Student-Course(user, course)
+studentid	(fk)	courseid(fk)
+
+University
+id	name	city		country		uniadmin_id
+
+College(university)
+uni_id	 id_college	name	
+
+Department(university)
+uni_id	dept_id	collegeid		name
+
+Course
+deptid	courseid		code	name
+
+
+Faculty-courses
+facultyid		courseid		
+
+
+Notes
+note_id	course_id	  studentid	note(blob)	approved/disapp
+
+
+Rating
+ratingid	noteid	studentid	   rating(/10)"""
+
