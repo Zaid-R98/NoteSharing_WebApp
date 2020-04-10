79d5dd4a21d13d3c2a55a7ad8c547259b951f89c
Zaid Rahman <zaidrahman0@gmail.com>
2020-04-10 18:25:19 +0400

    Fixed all error tags. Orientation is OK. Will work on Uni  list now

--- swe_project/forms.py
+++ swe_project/forms.py
@@ -1,11 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField,ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+
+
+def Namecheck(FlaskForm,field): 
+    if field.data.isdigit():
+        raise ValidationError('First name and Last name cannot contain any Numbers.')
+    
 
 class UserRegistrationForm(FlaskForm):
     #Things to add- email,pass,university_id,firstname,lastname
-    first_name= StringField('First Name',validators=[DataRequired(),Length(max=20)])
-    last_name= StringField('Last Name',validators=[DataRequired(),Length(max=20)])
+    first_name= StringField('First Name',validators=[DataRequired(),Length(max=20),Namecheck])
+    last_name= StringField('Last Name',validators=[DataRequired(),Length(max=20),Namecheck])
     email=StringField('Email ', validators=[Email(),DataRequired(),Length(max=40)])
     password=PasswordField('Password',validators=[DataRequired()])
     confirm_password=PasswordField('Confirm Passoword', validators=[DataRequired(),EqualTo('password')])
