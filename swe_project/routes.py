5c4c7f188ba507a2371708dc22e8db65f7c92514
Janos Gyerik <info+kronos@janosgyerik.com>
2020-04-12 14:14:20 +0200

    Fix login page

--- swe_project/routes.py
+++ swe_project/routes.py
@@ -15,11 +15,11 @@ def login():
         user = User.query.filter_by(email = form.email.data).first()
         if user and (user.password == form.password.data):
             print(" SUCCESFULL")
-            flash(f'Account has been logged in for {form.first_name.data} ', 'success')
+            flash(f'Account has been logged in for {form.email.data} ', 'success')
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('register_student.html'))
+            return redirect(url_for('profile'))
         else:
-            flash(f'Login has been unsuccessful. Email/password is wrong {form.first_name.data} ', 'success')
+            flash(f'Login has been unsuccessful. Email/password is wrong {form.email.data} ', 'success')
             print("NOT SUCCESFULL")
     return render_template('login.html',title ='Login', form=form)
 
