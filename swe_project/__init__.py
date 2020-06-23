c410f44ac8d1432ca22a3f5d48cd1ad5a6cc4b4c
Zaid Rahman <zaidrahman0@gmail.com>
2020-06-23 20:36:33 +0400

    Pre Cloud

--- swe_project/__init__.py
+++ swe_project/__init__.py
@@ -2,12 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager 
 from flask_wtf.csrf import CSRFProtect
-
-
+from swe_project.config import databse_uri,configkey
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SECRETKEYFORSWE'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+app.config['SECRET_KEY'] = configkey
+app.config['SQLALCHEMY_DATABASE_URI']=databse_uri
 db=SQLAlchemy(app)
 login_manager=LoginManager(app) 
 login_manager.login_view = 'login'
